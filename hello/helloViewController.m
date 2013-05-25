60f19d2b6402c6fd0749398a1247ddc0c3cefca2
holydiver <holydiver@holydivers-Mac-Pro.local>
2013-05-25 19:20:26 +0300

    commit 3

--- hello/helloViewController.m
+++ hello/helloViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	
-	
+	//backgroung sizes for different screen sizes
 	NSString *backgroundFilename = @"Background.png";
 	CGRect screenRect = [[UIScreen mainScreen] bounds];
 	if (screenRect.size.height == 568.0f)
@@ -26,21 +26,27 @@
 		backgroundFilename = [backgroundFilename stringByReplacingOccurrencesOfString:@".png" withString:@"-586h.png"];
 	}
 	
+	//loading image
 	self.imageView.image = [UIImage imageNamed:backgroundFilename];
+	
 
+	//custom label font
 	label.font = [UIFont fontWithName:@"Calcula" size:72.0];
+	//label background image
 	label.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Screen.png"]];
 	
+	//images for custom button
 	UIImage *btn = [UIImage imageNamed:@"Button.png"];
 	UIImage *btnH = [UIImage imageNamed:@"ButtonHighlighted.png"];
 	
+	//custom button
 	UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
 	
 	[button addTarget:self action:@selector(Increase) forControlEvents:UIControlEventTouchUpInside];
 	[button setBackgroundImage:btn forState:UIControlStateNormal];
 	[button setBackgroundImage:btnH forState:UIControlStateHighlighted];
 	[button setFrame:CGRectMake(122, 250, 65, 65)];
-	
+	//display custom button
 	[self.view addSubview:button];
 }
 
@@ -50,6 +56,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+//button touch down handler
 - (IBAction)Increase
 {
 	count++;
